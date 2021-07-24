22d3635d5f594f7f03d3c11d0d9b9685e0ce691f
thalyta <thalycomp@gmail.com>
2021-07-24 14:40:34 -0300

    estrutura da pag de produtos

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Box, Navigation, Form } from './styles';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
-  return <div>Products</div>;
+  const [{ cod, name, price }, setProduct] = useState({
+    cod: '',
+    name: '',
+    price: '',
+  });
+  const handleSubmit = () => {};
+  return (
+    <Container>
+      <Box>
+        <h1>Products</h1>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Digite o código do produto"
+            name={cod}
+            value={cod}
+            onChange={(e) => setProduct({ cod: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Digite o nome do produto"
+            name={name}
+            value={name}
+            onChange={(e) => setProduct({ name: e.target.value })}
+          />
+          <input
+            type="number"
+            min="1"
+            step="any"
+            placeholder="Digite o preço do produto"
+            name={price}
+            value={price}
+            onChange={(e) => setProduct({ price: e.target.value })}
+          />
+          <button type="submit">CADASTRAR</button>
+        </Form>
+        <Navigation>
+          <Link to="/">DASHBOARD</Link>
+          <Link to="/clientes">CADASTRAR CLIENTES</Link>
+        </Navigation>
+      </Box>
+    </Container>
+  );
 };
 
 export default Products;
---
+++ src/pages/Products/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const Box = styled.main`
+  width: 90%;
+  max-width: 900px;
+  border-radius: 4px;
+  background: #fff;
+  padding: 10px;
+  border: 1px solid #e1e4e8;
+  text-align: center;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+
+  input {
+    width: 100%;
+    padding: 15px;
+    background: #eee;
+    font-size: 14px;
+    border: 0;
+    margin-bottom: 10px;
+  }
+
+  button {
+    padding: 14px;
+    border-radius: 4px;
+    border: none;
+    background: #2166d6;
+    color: #fff;
+    margin: 2px;
+    cursor: pointer;
+    flex: 1;
+  }
+`;
+
+export const Navigation = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid #ddd;
+  padding: 20px;
+
+  a {
+    padding: 14px;
+    border-radius: 4px;
+    border: none;
+    background: #2166d6;
+    color: #fff;
+    margin: 2px;
+    cursor: pointer;
+    flex: 1;
+  }
+`;
